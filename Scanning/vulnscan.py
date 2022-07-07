47b796884af26913d06bfbbaec6be0006093546e
Deepak <nagarkotideepak9843@gmail.com>
2022-07-07 22:10:40 +0545

    Edit Vulnscan file

--- Scanning/vulnscan.py
+++ Scanning/vulnscan.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python
 
+from asyncore import read
 import socket
 import os
 import sys
+
+def retBanner(ip, port):
+    try:
+        socket.setdefaulttimeout(2)
+        s = socket.socket()
+        s.connect((ip,port))
+        banner = s.recv(1024)
+        return banner
+    except:
+        return
+
+def checkVulns(banner, filename):
+    f = open(filename, "r")
+    for line in f.readlines():
+        if line.strip("\n") in str(banner):
+            print("[+] Server is Vulnerable:"  + str(banner))
+
 
 def main():
     if len(sys.argv) == 2:
@@ -19,12 +37,12 @@ def main():
     portlist = [21,22,25,80,110,443,445]
 
     # to all hosts in network
-    for x in range(1,255):
+    for x in range(74, 77):
         ip = "192.168.1." + str(x)
         for port in portlist:
             banner = retBanner(ip,port)
             if banner:
-                print("[+] " + ip + "/" + str(port) + ":" + banner)
+                print("[+] " + ip + "/" + str(port) + ":" + str(banner))
                 checkVulns(banner, filename)
 
 
